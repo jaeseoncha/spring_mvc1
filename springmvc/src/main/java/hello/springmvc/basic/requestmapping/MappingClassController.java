62602202b91d4d51ddaf2da25924a1b0e8b92a7c
jaeseoncha <jbsg0727@naver.com>
2022-10-06 01:33:43 +0900

    requestParam test

---
+++ springmvc/src/main/java/hello/springmvc/basic/requestmapping/MappingClassController.java
@@ -0,0 +1,33 @@
+package hello.springmvc.basic.requestmapping;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/mapping/users")
+public class MappingClassController {
+
+    @GetMapping
+    public String user(){
+
+        return "getUsers";
+    }
+
+    @PostMapping
+    public String addUser(){
+        return"postUser";
+    }
+
+    @GetMapping("/{userId}")
+    public String findUser(@PathVariable String userId){
+        return"getUserId"+userId;
+    }
+    @PatchMapping("/{userId}")
+    public String updateUser(@PathVariable String userId){
+        return"updateUser"+userId;
+    }
+
+    @DeleteMapping("/{userId}")
+    public String deleteUser(@PathVariable String userId){
+        return"deleteUser"+userId;
+    }
+}
